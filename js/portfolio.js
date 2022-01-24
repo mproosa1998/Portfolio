f0ce71aa69a9b9e1f748d85959a640a5945040f7
mroosa1998 <56371790+mroosa1998@users.noreply.github.com>
2022-01-24 14:34:20 -0500

    updated portfolio website with JS code

---
+++ js/portfolio.js
@@ -0,0 +1,46 @@
+// open and close the contact form
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+// displays the first image in the slideshow when the page loads
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// changes the slide when the left or right arrows are clicked
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// changes the slide when the dots are clicked
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+// controls and organizes slide show components
+function showSlides(n) {
+    var slides = document.getElementsByClassName("mySlides"); 
+    var dots = document.getElementsByClassName("dot"); 
+    if (n > slides.length) {slideIndex = 1}; 
+    if (n < 1) {slideIndex = slides.length};
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");  
+           
+    }
+    slides[slideIndex - 1].style.display = "block"; 
+    dots[slideIndex - 1].className += " active";
+} 
+
+// This code will create close the contact form when the user clicks off of it
+document.addEventListener("click", function(event){
+    if (event.target.matches(".cancel") || !event.target.closest(".form-popup") && !event.target.closest(".Pop_Up_Button") && !event.target.closest(".contact")){
+        closeForm()
+    }
+}, false )
